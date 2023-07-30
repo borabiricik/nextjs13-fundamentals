65de32cc6420557976a23ab5089f0e8af9413cc6
borabiricik <bora.bir496@gmail.com>
2023-07-30 23:25:19 +0300

    feat(routing): Parallel Routing improvements

--- 1-routing/g_parallel-routes/app/dashboard/layout.tsx
+++ 1-routing/g_parallel-routes/app/dashboard/layout.tsx
@@ -6,13 +6,31 @@ interface Props extends PropsWithChildren {
   todos?: ReactElement;
 }
 
-const DashboardLayout = ({ children, analytics, posts, todos }: Props) => {
+const DashboardLayout = async ({
+  children,
+  analytics,
+  posts,
+  todos,
+}: Props) => {
+  let isAuthenticated: boolean = true; //! Change me !
   return (
     <div>
       <div className="grid grid-cols-3 items-start space-x-6">
         {posts}
         <div className="col-span-1 flex flex-col space-y-6">
-          {analytics}
+          <p>
+            We‘ll do dummy authentication in this section. We‘ll wait for few
+            seconds and change authenticated state. For changing the state,
+            change isAuthenticated variable to true/false and observe the
+            changes below 🥵
+          </p>
+          {isAuthenticated ? (
+            analytics
+          ) : (
+            <span className="text-red-400">
+              You‘re not authenticated to see this part !
+            </span>
+          )}
           {todos}
         </div>
       </div>
